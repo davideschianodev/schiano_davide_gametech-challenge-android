109e3f109c43909d775469d5c2b9b2cc177ba82a
davideschianodev <davideschiano.dev@gmail.com>
2024-12-10 00:04:14 +0100

    feat: implemented the "showing the touched notification's data" + updated icons

--- lib/src/main/java/com/schianodavide/miniclip/LocalPushNotificationReceiver.java
+++ lib/src/main/java/com/schianodavide/miniclip/LocalPushNotificationReceiver.java
@@ -1,11 +1,15 @@
 package com.schianodavide.miniclip;
 
+import android.app.ActivityManager;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
 import androidx.core.app.NotificationCompat;
+
+import java.util.List;
 
 public class LocalPushNotificationReceiver extends BroadcastReceiver {
     @Override
@@ -15,17 +19,47 @@ public class LocalPushNotificationReceiver extends BroadcastReceiver {
         String message = intent.getStringExtra("message");
         int notificationIcon = intent.getIntExtra("notificationIcon", android.R.drawable.ic_dialog_info);
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "local_push_notifications_channel")
-                .setSmallIcon(notificationIcon)
-                .setContentTitle(title)
-                .setContentText(message)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-                .setAutoCancel(true);
+        Intent unityIntent = context.getPackageManager().getLaunchIntentForPackage(context.getPackageName());
+        if (unityIntent != null) {
+            unityIntent.putExtra("notificationTitle", title);
+            unityIntent.putExtra("notificationMessage", message);
+            unityIntent.putExtra("notificationIcon", notificationIcon);
+            unityIntent.setAction("OPEN_APP_FROM_NOTIFICATION");
 
-        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        if (manager != null) {
-            manager.notify(notificationId, builder.build());
+            if (isAppRunning(context)) {
+                unityIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+            } else {
+                unityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            }
+
+            PendingIntent pendingIntent = PendingIntent.getActivity(
+                    context, notificationId, unityIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "local_push_notifications_channel")
+                    .setSmallIcon(notificationIcon)
+                    .setContentTitle(title)
+                    .setContentText(message)
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent);
+
+            NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            if (manager != null) {
+                manager.notify(notificationId, builder.build());
+            }
         }
+    }
+
+    private boolean isAppRunning(Context context) {
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> tasks = activityManager.getRunningTasks(Integer.MAX_VALUE);
+
+        for (ActivityManager.RunningTaskInfo task : tasks) {
+            if (context.getPackageName().equalsIgnoreCase(task.baseActivity.getPackageName())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
